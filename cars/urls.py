6a1c7d8d664ec55b1e1a54b79a386fa9237fcb24
AdamMisiak <adammisiak3@gmail.com>
2020-09-07 12:29:55 +0200

    Views refactored to class-based views

--- cars/urls.py
+++ cars/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from cars import views
 
 
 urlpatterns = [
-    path('cars/', views.cars_list),
-    path('cars/<int:pk>/', views.car_detail),
+    path('cars/', views.CarsList.as_view()),
+    path('cars/<int:pk>/', views.CarDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- cars/views.py
+++ cars/views.py
@@ -1,51 +1,52 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from cars.models import Car
 from cars.serializers import CarSerializer
 
 
-@csrf_exempt
-def cars_list(request):
+class CarsList(APIView):
     """
     List all code cars, or create a new car.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         cars = Car.objects.all()
         serializer = CarSerializer(cars, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = CarSerializer(data=data)
+    def post(self, request, format=None):
+        serializer = CarSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def car_detail(request, pk):
+class CarDetail(APIView):
     """
-    Retrieve, update or delete a code snippet.
+    Retrieve, update or delete a car.
     """
-    try:
-        car = Car.objects.get(pk=pk)
-    except Car.DoesNotExist:
-        return HttpResponse(status=404)
+    def get_object(self, pk):
+        try:
+            return Car.objects.get(pk=pk)
+        except Car.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        car = self.get_object(pk)
         serializer = CarSerializer(car)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = CarSerializer(car, data=data)
+    def put(self, request, pk, format=None):
+        car = self.get_object(pk)
+        serializer = CarSerializer(car, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        car = self.get_object(pk)
         car.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
