d1b793e699cf0ea06bb7edfeea74a642700b93fb
AdamMisiak <adammisiak3@gmail.com>
2020-09-13 15:47:50 +0200

    Database changed to postgresql

--- Car_REST_API/settings.py
+++ Car_REST_API/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Car_REST_API.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'car_rest_api',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
